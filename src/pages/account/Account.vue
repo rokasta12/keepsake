3832d5aa0685a2e028ef1021f47d999a75df5c5e
Mehmet Firat Aksoy <74587358+mfirataksoy@users.noreply.github.com>
2023-02-05 03:42:48 -0600

    new beatiful account page
    beautiful account page

--- src/pages/account/Account.vue
+++ src/pages/account/Account.vue
@@ -55,67 +55,45 @@ export default {
 </script>
 
 <template>
-  <div class="p-6 bg-white rounded-lg shadow-md">
-    <h2 class="text-2xl font-bold mb-4">
-      Account Information
-    </h2>
-    <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
-      <div class="mb-4 md:mb-0">
-        <label class="text-lg font-medium mb-2">First Name</label>
-        <input
-          v-model="firstName"
-          class="border border-gray-300 p-2 rounded-lg"
-          type="text"
-        >
-      </div>
-      <div class="mb-4 md:mb-0">
-        <label class="text-lg font-medium mb-2">Last Name</label>
-        <input
-          v-model="lastName"
-          class="border border-gray-300 p-2 rounded-lg"
-          type="text"
-        >
-      </div>
-      <div class="mb-4 md:mb-0">
-        <label class="text-lg font-medium mb-2">Email</label>
-        <input
-          v-model="email"
-          class="border border-gray-300 p-2 rounded-lg"
-          type="email"
-        >
-      </div>
-      <div class="mb-4 md:mb-0">
-        <label class="text-lg font-medium mb-2">Birth Date</label>
-        <input
-          v-model="birthDate"
-          class="border border-gray-300 p-2 rounded-lg"
-          type="date"
-        >
-      </div>
-      <div class="mb-4 md:mb-0">
-        <label class="text-lg font-medium mb-2">Phone Number</label>
-        <input
-          v-model="phoneNumber"
-          class="border border-gray-300 p-2 rounded-lg"
-          type="tel"
-        >
-      </div>
-      <div class="mb-4 md:mb-0">
-        <label class="text-lg font-medium mb-2">Profile Picture</label>
+  <div>
+    <h1 class="text-2xl mt-10" font-bold dark:text-white>
+      Edit Your Account Details
+    </h1>
+  </div>
+  <div class="h-2xl">
+    <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex justify-center">
+      <div class="flex flex-col gap-1 max-w-xl" w="300px">
+        <label class=" text-gray-700 text-left" for="name">
+          First name
+        </label>
+        <input v-model="firstName" class="border border-gray-300 p-2 rounded-lg" type="text">
+        <label class=" text-gray-700 text-left" for="name">
+          Last name
+        </label>
+        <input v-model="lastName" class="border border-gray-300 p-2 rounded-lg" type="text">
+        <label class=" text-gray-700 text-left" for="email">
+          Email
+        </label>
+        <input v-model="email" class="border border-gray-300 p-2 rounded-lg" type="email">
+        <label class=" text-gray-700 text-left" for="birthDate">
+          Birth Date
+        </label>
+        <input v-model="birthDate" class="border border-gray-300 p-2 rounded-lg" type="text">
+        <label class=" text-gray-700 text-left" for="phoneNumber">
+          Phone Number
+        </label>
+        <input v-model="phoneNumber" class="border border-gray-300 p-2 rounded-lg" type="text">
+        <label class=" text-gray-700 text-left" for="profilePicture">
+          Profile Picture
+        </label>
         <input
           class="border border-gray-300 p-2 rounded-lg"
           type="file"
           v-on="profilePicture"
-        >
+        >        <button class="btn mt-2 text-lg" @click="submitForm">
+          Update Information
+        </button>
       </div>
-    </div>
-    <div class="flex justify-end mt-4">
-      <button
-        class="bg-indigo-500 hover:bg-indigo-600 text-white font-medium py-2 px-4 rounded-lg"
-        @click="submitForm"
-      >
-        Save Changes
-      </button>
     </div>
   </div>
 </template>
